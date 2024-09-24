be1e69ddbf3e2201a05b550ad3e1639e67309bd5
Brendon Busic <32741868+BusicMusic@users.noreply.github.com>
2024-09-23 20:10:54 -0400

    Add files via upload

--- KGC-Calc.js
+++ KGC-Calc.js
@@ -378,7 +378,7 @@ function calcLeonhardtSkill(){
 }
 
 function calcPriyaSkill(){
-    let Hero="Leonhardt";
+    let Hero="Priya";
     //getting the value of the numerical inputs by the user
     let atkBonus= document.getElementById(Hero+ "AtkBonus").value;
     let spellBonus= document.getElementById(Hero+ "SpellBonus").value;
@@ -387,13 +387,13 @@ function calcPriyaSkill(){
     
     //setting the new values of the stats after taking the bonuses into account
     let SkillDmg= 0;
-    let hp=1770+(1770*(hpBonus/100));
-    let spellAtk=89+(89*(spellBonus/100));
+    let hp=708+(708*(hpBonus/100));
+    let spellAtk=384+(384*(spellBonus/100));
     let atk=89+(89*(atkBonus/100));
-    let asp=91+(91*(aspBonus/100));
+    let asp=100+(100*(aspBonus/100));
     
     if (document.getElementById(Hero+"lv4Passive").checked){
-        //Leonhardt's passive skills do not impact his skill
+        //Priya's passive skills do not impact her skill
     }
 
     if (document.getElementById(Hero+"A0").checked){
@@ -402,17 +402,17 @@ function calcPriyaSkill(){
         let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
         let output= finalSkillDmg.toLocaleString('en');
         
-        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Smite</i> attack");
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Blizzard</i> attack");
     }
     else if (document.getElementById(Hero+"A1").checked){
         SkillDmg=30+spellAtk;
-        let SkillDmg2=spellAtk;
+        let SkillDmg2=spellAtk*0.3;
 
         let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
         let output= finalSkillDmg.toLocaleString('en');
         let output2= SkillDmg2.toLocaleString('en');
         
-        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Smite</i> attack</br><b>"+ output2+ "</b> protection gained per enemy hit with <i>Barrier</i> active");
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Blizzard</i> attack</br><b>"+ output2+ "</b> damage per second for 3 seconds to enemies within range of <i>Barrier</i>");
     }
     else if (document.getElementById(Hero+"A2").checked){
         SkillDmg=30+spellAtk;
@@ -420,20 +420,268 @@ function calcPriyaSkill(){
         let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
         let output= finalSkillDmg.toLocaleString('en');
         
-        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Smite</i> attack");
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Blizzard</i> attack");
     }
     return 7;
 }
 
-function calcDanielSkill(){}
+function calcDanielSkill(){
+    let Hero="Daniel";
+    //getting the value of the numerical inputs by the user
+    let atkBonus= document.getElementById(Hero+ "AtkBonus").value;
+    let spellBonus= document.getElementById(Hero+ "SpellBonus").value;
+    let aspBonus= document.getElementById(Hero+ "AspBonus").value;
+    let hpBonus= document.getElementById(Hero+ "HpBonus").value;
+    
+    //setting the new values of the stats after taking the bonuses into account
+    let SkillDmg= 0;
+    let hp=1475+(1475*(hpBonus/100));
+    let spellAtk=118+(118*(spellBonus/100));
+    let atk=118+(118*(atkBonus/100));
+    let asp=100+(100*(aspBonus/100));
+    
+    if (document.getElementById(Hero+"lv8Passive").checked){
+        hp=(hp+(hp*0.2));
+    }
 
-function calcMaraSkill(){}
+    if (document.getElementById(Hero+"A0").checked){
+        SkillDmg=50+spellAtk;
 
-function calcChungAhSkill(){}
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Judgement of Light</i> attack");
+    }
+    else if (document.getElementById(Hero+"A1").checked){
+        SkillDmg=50+spellAtk;
+        let SkillDmg2=atk*3;
 
-function calcLilySkill(){}
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Judgement of Light</i> attack</br><b>"+ output2+ "</b> damage to each enemy on the first normal attack with <i>Divine Bash</i>");
+    }
+    else if (document.getElementById(Hero+"A2").checked){
+        SkillDmg=50+spellAtk;
+        let SkillDmg2=hp*0.3;
 
-function calcJolSkill(){}
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Judgement of Light</i> attack</br><b>"+ output2+ "</b> health regen per second for 3 seconds with <i>Self-Recovery</i>");
+    }
+    return 8;
+}
+
+function calcMaraSkill(){
+    let Hero="Mara";
+    //getting the value of the numerical inputs by the user
+    let atkBonus= document.getElementById(Hero+ "AtkBonus").value;
+    let spellBonus= document.getElementById(Hero+ "SpellBonus").value;
+    let aspBonus= document.getElementById(Hero+ "AspBonus").value;
+    let hpBonus= document.getElementById(Hero+ "HpBonus").value;
+    
+    //setting the new values of the stats after taking the bonuses into account
+    let SkillDmg= 0;
+    let hp=826+(826*(hpBonus/100));
+    let spellAtk=354+(354*(spellBonus/100));
+    let atk=77+(77*(atkBonus/100));
+    let asp=100+(100*(aspBonus/100));
+    
+    if (document.getElementById(Hero+"lv8Passive").checked){
+        //Mara's passive abilities don't affect her skill
+    }
+
+    if (document.getElementById(Hero+"A0").checked){
+        SkillDmg=spellAtk;
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Backstab</i> attack");
+    }
+    else if (document.getElementById(Hero+"A1").checked){
+        SkillDmg=spellAtk;
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Backstab</i> attack");
+    }
+    else if (document.getElementById(Hero+"A2").checked){
+        SkillDmg=spellAtk;
+        let SkillDmg2= hp-(hp*0.2);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Backstab</i> attack");
+    }
+    return 10;
+}
+
+function calcChungAhSkill(){
+    let Hero="ChungAh";
+    //getting the value of the numerical inputs by the user
+    let atkBonus= document.getElementById(Hero+ "AtkBonus").value;
+    let spellBonus= document.getElementById(Hero+ "SpellBonus").value;
+    let aspBonus= document.getElementById(Hero+ "AspBonus").value;
+    let hpBonus= document.getElementById(Hero+ "HpBonus").value;
+    
+    //setting the new values of the stats after taking the bonuses into account
+    let SkillDmg= 0;
+    let hp=1475+(1475*(hpBonus/100));
+    let spellAtk=94+(94*(spellBonus/100));
+    let atk=89+(89*(atkBonus/100));
+    let asp=125+(125*(aspBonus/100));
+    
+    if (document.getElementById(Hero+"lv8Passive").checked){
+        //Chung Ah's passive abilities don't affect her skill damage
+    }
+
+    if (document.getElementById(Hero+"A0").checked){
+        SkillDmg=(10+spellAtk);
+        let SkillDmg2=(SkillDmg*2.5);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Horizontal Dragon Edge</i> attack</br><b>"+ output2+ "</b> per <i>Azure Dragon Slash</i> attack");
+    }
+    else if (document.getElementById(Hero+"A1").checked){
+        SkillDmg=(10+spellAtk);
+        let SkillDmg2=(SkillDmg*2.5);
+        let SkillDmg3=SkillDmg2+(SkillDmg2*0.4);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let finalSkillDmg3=Math.round(SkillDmg3*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        let output3= finalSkillDmg3.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Horizontal Dragon Edge</i> attack</br><b>"+ output2+ "</b> per <i>Azure Dragon Slash</i> attack</br><b>"+ output3+ "</b> damage per instant <i>Azure Dragon Slash</i> from <i>Relentless Slash</i> awakening");
+    }
+    else if (document.getElementById(Hero+"A2").checked){
+        SkillDmg=(10+spellAtk);
+        let SkillDmg2=(SkillDmg*2.5);
+        let SkillDmg3=SkillDmg*10;
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let finalSkillDmg3=Math.round(SkillDmg3*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        let output3= finalSkillDmg3.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Horizontal Dragon Edge</i> attack</br><b>"+ output2+ "</b> per <i>Azure Dragon Slash</i> attack</br><b>"+ output3+ "</b> damage per instant <i>Advent of Thunder Dragon</i>");
+    }
+    return 11;
+}
+
+function calcLilySkill(){
+    let Hero="Lily";
+    //getting the value of the numerical inputs by the user
+    let atkBonus= document.getElementById(Hero+ "AtkBonus").value;
+    let spellBonus= document.getElementById(Hero+ "SpellBonus").value;
+    let aspBonus= document.getElementById(Hero+ "AspBonus").value;
+    let hpBonus= document.getElementById(Hero+ "HpBonus").value;
+    
+    //setting the new values of the stats after taking the bonuses into account
+    let SkillDmg= 0;
+    let hp=590+(590*(hpBonus/100));
+    let spellAtk=295+(295*(spellBonus/100));
+    let atk=59+(59*(atkBonus/100));
+    let asp=100+(100*(aspBonus/100));
+    
+    if (document.getElementById(Hero+"lv8Passive").checked){
+        //Lily's passive abilities don't affect her skill damage
+    }
+
+    if (document.getElementById(Hero+"A0").checked){
+        SkillDmg=(20+spellAtk);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Vitality of Nature</i> use</br>____ damage per <i>Charge!</i> attack");
+    }
+    else if (document.getElementById(Hero+"A1").checked){
+        SkillDmg=(20+spellAtk);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Vitality of Nature</i> use</br>____ damage per <i>Charge!</i> attack");
+    }
+    else if (document.getElementById(Hero+"A2").checked){
+        SkillDmg=(20+spellAtk);
+        let SkillDmg2=spellAtk;
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Vitality of Nature</i> use</br><b>"+ output2+ "</b> damage per <i>Charge!</i> attack");
+    }
+    return 11;
+}
+
+function calcJolSkill(){
+    let Hero="Jol";
+    //getting the value of the numerical inputs by the user
+    let atkBonus= document.getElementById(Hero+ "AtkBonus").value;
+    let spellBonus= document.getElementById(Hero+ "SpellBonus").value;
+    let aspBonus= document.getElementById(Hero+ "AspBonus").value;
+    let hpBonus= document.getElementById(Hero+ "HpBonus").value;
+    
+    //setting the new values of the stats after taking the bonuses into account
+    let SkillDmg= 0;
+    let hp=1298+(1298*(hpBonus/100));
+    let spellAtk=118+(118*(spellBonus/100));
+    let atk=89+(89*(atkBonus/100));
+    let asp=100+(100*(aspBonus/100));
+    
+    if (document.getElementById(Hero+"lv8Passive").checked){
+        //Jol's passive abilities don't affect his skill damage
+    }
+
+    if (document.getElementById(Hero+"A0").checked){
+        SkillDmg=(20+spellAtk);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Vitality of Nature</i> use</br>____ damage per <i>Charge!</i> attack");
+    }
+    else if (document.getElementById(Hero+"A1").checked){
+        SkillDmg=(20+spellAtk);
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Vitality of Nature</i> use</br>____ damage per <i>Charge!</i> attack");
+    }
+    else if (document.getElementById(Hero+"A2").checked){
+        SkillDmg=(20+spellAtk);
+        let SkillDmg2=spellAtk;
+
+        let finalSkillDmg=Math.round(SkillDmg*100)/100; //rounding to nearest 100th
+        let finalSkillDmg2=Math.round(SkillDmg2*100)/100; //rounding to nearest 100th
+        let output= finalSkillDmg.toLocaleString('en');
+        let output2= finalSkillDmg2.toLocaleString('en');
+        
+        document.getElementById(Hero+"SkillDmg").innerHTML=("<b>"+ output+ "</b> per <i>Vitality of Nature</i> use</br><b>"+ output2+ "</b> damage per <i>Charge!</i> attack");
+    }
+    return 12;
+}
 
 function calcRenSkill(){}
 
